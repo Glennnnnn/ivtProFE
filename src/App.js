48361d1178a15a2ab6d1fd385c5ca7f580ff4b84
glennnnnn <ljlsjcnh649692327@gmail.com>
2023-08-29 21:31:50 +0800

    【Glenn】【init】【290823】

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Login from './pages/Login/index.js'
+import Layout from './pages/Layout/index.js'
+
+import { Button } from 'antd'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <button type="primary">Primary Button</button>
+        <Routes>
+          <Route path="/" element={<Layout />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
