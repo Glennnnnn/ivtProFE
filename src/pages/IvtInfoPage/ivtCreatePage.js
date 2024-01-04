b1cfc51e7d570d60f835f142bd38a0db7389723a
glennnnnn <ljlsjcnh649692327@gmail.com>
2024-01-04 11:36:34 +0800

    [CreatePage]

--- src/pages/IvtInfoPage/ivtCreatePage.js
+++ src/pages/IvtInfoPage/ivtCreatePage.js
@@ -24,133 +24,15 @@ const IvtCreatePage = () => {
   const [form] = Form.useForm();
   const { Content } = Layout;
 
-  const [existTagList, setExistTagList] = useState([])
-  const [inputTagValue, setInputTagValue] = useState();
-  const [openTagOpts, setOpenTagOpts] = useState(false)
-  const [ivtClassOpts, setIvtClassOpts] = useState();
-
-  const [inputIvtClassValue, setInputIvtClassValuee] = useState();
-
-  useEffect(() => {
-    const queryBaseData = async () => {
-      let resJson = await http.get("/ivtClass/queryIvtClassList")
-      let res = JSON.parse(JSON.stringify(resJson.data.data))
-      let ivtClassOptions = []
-      for (var i = 0; i < res.length; i++) {
-        ivtClassOptions.push({
-          label: res[i]["ivtClassName"],
-          value: res[i]["ivtClassId"]
-        })
-
-      }
-      setIvtClassOpts(ivtClassOptions);
-    }
-
-    queryBaseData()
-  }, [])
-
-  const onTagValueInputChange = (event) => {
-
-    setInputTagValue(event.target.value);
-  };
-  //ivt class info change related functions
-  const ivtClassfilterOption = (input, option) => {
-    (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
-  }
-
-  const onIvtClassChange = (value) => {
-    console.log(`selected ${value}`);
-  };
-
-  const addItem = (e) => {
-    e.preventDefault();
-    //setItems([...items, name || `New item ${index++}`]);
-    console.log(e.target.value)
-    setInputTagValue("")
-  };
   const onFinish = async (values) => {
     console.log('Received values of form: ', values);
-    await http.post("/ivt/updateIvt", { values })
+    //await http.post("/ivt/updateIvt", { values })
   };
-
-  const generateInitFormData = () => {
-    let result = {
-      'ivtClassName': ivtClassOpts
-    }
-    console.log(result)
-    return result
-  }
-
-  const renderTagForms = () => {
-    return (
-      Object.keys(existTagList).map(tagName => {
-        //console.log(existTagList[tagName])
-        return (
-
-          <Form.Item lable={tagName} name={["tags", tagName]} key={tagName} style={{ width: '300px' }}>
-            <Select
-              allowClear
-              style={{
-                width: '280px',
-              }}
-              placeholder={tagName}
-              options={processTagOptions(existTagList[tagName])}
-              dropdownRender={(menu) => (
-                <>
-                  {menu}
-                  <Divider
-                    style={{
-                      margin: '8px 0',
-                    }}
-                  />
-                  <Space
-                    style={{
-                      padding: '0 8px 4px',
-                    }}
-                  >
-                    <Input
-                      placeholder="Please enter item"
-                      style={{ width: '150px' }}
-                      value={inputTagValue}
-                      onChange={onTagValueInputChange}
-                      onKeyDown={(e) => e.stopPropagation()}
-                    />
-                    <Button type="text" icon={<PlusOutlined />} onClick={addItem} style={{ width: '20px' }}>
-                      Add tag
-                    </Button>
-                  </Space>
-                </>
-              )}
-            >
-            </Select>
-          </Form.Item>
-        )
-      })
-    )
-  }
-
-  const processTagOptions = (tagValues) => {
-    let tagValueOptions = []
-    //console.log(tagValues)
-    for (var i = 0; i < tagValues.length; i++) {
-      for (let obj in tagValues[i]) {
-        tagValueOptions.push({
-          label: tagValues[i][obj],
-          value: obj
-        })
-      }
-    }
-    //console.log(tagValueOptions)
-    return tagValueOptions
-  }
 
   const clickTest = () => {
-    console.log(ivtClassOpts)
+    console.log()
   }
 
-  const onIvtClassValueInputChange = (event) => {
-    setInputIvtClassValuee(event.target.value);
-  };
 
   return (
     <Layout>
@@ -201,11 +83,6 @@ const IvtCreatePage = () => {
                 width: '280px',
               }}
               placeholder="Inventory Name"
-              // onChange={handleChange}
-              options={ivtClassOpts}
-              filterOption={ivtClassfilterOption}
-              onChange={onIvtClassChange}
-            // onSearch={onSearch}
             >
             </Select>
           </Form.Item>
@@ -228,11 +105,6 @@ const IvtCreatePage = () => {
           <Form.Item name="ivtNote" label="Note">
             <TextArea rows={4} />
           </Form.Item>
-          {openTagOpts && <Form.Item label="tags">
-            <Space size={[0, 8]} wrap>
-              renderTagForms()
-            </Space>
-          </Form.Item>}
           <Form.Item label="Upload" valuePropName="fileList" getValueFromEvent={normFile}>
             <Upload action="/upload.do" listType="picture-card">
               <div>
