7f84c014dd3a3df188286de46978267b9020862e
glennnnnn <ljlsjcnh649692327@gmail.com>
2024-01-07 13:24:47 +0800

    proxy settings

--- craco.config.js
+++ craco.config.js
@@ -9,16 +9,4 @@ module.exports = {
       '@': path.resolve(__dirname, 'src')
     }
   },
-  devServer: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://3.25.98.192:8081',
-        changeOrigin: true,
-        pathRewrite: {
-          "^/api": ''
-        }
-      },
-    }
-  }
 }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
-  app.use("/api", // 由于请求前后端此时都为3000端口，由于区分请求后端的前缀
+  app.use("/api",
     createProxyMiddleware({
-      target: "http://localhost:8081",// 后端服务器地址
+      target: "http://3.25.98.192:8081",
       changeOrigin: true,
       pathRewrite: {
         "^/api": ""
-      } // 重写地址，将附加的api前缀去除给到后端地址
-      // 若后端请求地址为 http://127.0.0.1:8080/myprocess/
-      // 此时请求代理服务器地址应为 http://127.0.0.1:8080/api/myprocess/
-      // 此处选项就是去除api/这个前缀的
+      }
     }))
 };
